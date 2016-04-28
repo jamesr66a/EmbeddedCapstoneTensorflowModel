f2bd14bd7fee928bb1d77e57d99570dde329fd8d
James Reed <jamesr66@vt.edu>
2016-04-27 20:57:19 -0400

    serialize

--- data_extraction.py
+++ data_extraction.py
@@ -1,16 +1,31 @@
-from generated.feature_pb2 import Feature
-from generated.example_pb2 import Example
+X = []
+Y = []
 
 with open('apr25_1.csv') as f:
     lines = f.readlines()
+    features = set()
     for line in lines:
         toks = line.split(',')
-        features = zip(toks[3::2], toks[4::2])
-        x = Example()
-        x.features.feature['x'].float_list.value.append(float(toks[0]))
-        x.features.feature['y'].float_list.value.append(float(toks[1]))
-        x.features.feature['yaw'].float_list.value.append(float(toks[2]))
-        for key, value in features:
-          x.features.feature[key].int64_list.value.append(int(value))
-        print x
-        break
+        for ft in toks[3::2]:
+          if ft != '\n':
+            features.add(ft)
+
+    feat_list = list(features)
+
+    X = []
+    Y = []
+    for line in lines:
+      toks = line.split(',')
+      x_vec = [-100]*len(features)
+      for key, value in zip(toks[3::2], toks[4::2]):
+        if key != '\n':
+          x_vec[feat_list.index(key)] = int(value)
+      X += [x_vec]
+      y_vec = [float(i) for i in toks[0:3]]
+      Y += [y_vec]
+
+with open('outfile', 'w') as f:
+  for row in X:
+    for col in row:
+      f.write('%s,' % col)
+    f.write('\n')
--- model_train.py
+++ model_train.py
@@ -1,31 +1,69 @@
-import pandas
+import os
 import skflow
+from skflow.addons.config_addon import ConfigAddon
 import sys
+
+import numpy as np
 
 from sklearn.cross_validation import train_test_split
 from sklearn.metrics import r2_score, mean_absolute_error
 
-data = pandas.read_csv('out.txt')
+X = []
+Y = []
 
-y, X = data['x'], data[data.columns[2:-1]].fillna(-100)
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+with open('apr25_1.csv') as f:
+    lines = f.readlines()
+    features = set()
+    for line in lines:
+        toks = line.split(',')
+        for ft in toks[3::2]:
+          if ft != '\n':
+            features.add(ft)
+                                       
+    feat_list = list(features)
 
-if len(sys.argv) > 1:
+    X = []
+    Y = []
+    for line in lines:
+      toks = line.split(',')
+      x_vec = [-100]*len(features)
+      for key, value in zip(toks[3::2], toks[4::2]):
+        if key != '\n':
+          x_vec[feat_list.index(key)] = int(value)
+      X += [x_vec]
+      y_vec = [float(i) for i in toks[0:2]]
+      Y += [y_vec]
+
+X_np = np.array(X, dtype=np.int32)
+Y_np = np.array(Y, dtype=np.double)
+
+X_train, X_test, y_train, y_test = train_test_split(X_np, Y_np, test_size=0.2, random_state=42)
+
+if os.path.exists(sys.argv[1]):
   print sys.argv[1]
   regressor = skflow.TensorFlowDNNRegressor.restore(sys.argv[1])
 else:
   regressor = skflow.TensorFlowDNNRegressor(
-      hidden_units=[50, 50, 50],
-      batch_size=128,
-      steps=500000,
-      learning_rate=0.005)
-  try:
+      hidden_units=[37, 37, 37],
+      batch_size=1000,
+      steps=1000,
+      learning_rate=0.0005,
+      continue_training=True,
+      config_addon = ConfigAddon(num_cores=9))
+
+if not os.path.exists(sys.argv[1]):
+  os.makedirs(sys.argv[1])
+
+with open(sys.argv[1] + "/logfile.txt", "a") as f:
+  while True:
     regressor.fit(X_train, y_train, logdir='training')
-  except KeyboardInterrupt:
-    pass
+    regressor.save(sys.argv[1])
 
-y_hat = regressor.predict(X_test)
-print 'mean absolute error: ', mean_absolute_error(y_hat, y_test)
-print 'r^2: ', r2_score(y_hat, y_test)
+    y_hat = regressor.predict(X_test)
+    mae = mean_absolute_error(y_hat, y_test)
+    r2 = r2_score(y_hat, y_test) 
+    print 'mean absolute error: ', mean_absolute_error(y_hat, y_test)
+    print 'r^2: ', r2_score(y_hat, y_test)
+    f.write('{},{}\n'.format(mae, r2))
+    f.flush()
 
-regressor.save('3hidden50_gaussian3')
